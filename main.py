5670872e4ad6d6e2933d576142453d770a67f837
Polvory <32514592+Polvory@users.noreply.github.com>
2025-04-27 20:42:35 +0300

    add Transactions

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from fastapi import FastAPI
 import db  # подключение произойдёт автоматически
 from db import Base, engine  # Импорт движка и базового класса
-from models import User      # Импортируй модель, чтобы SQLAlchemy "увидел" её
-
+from models import User, Transactions      # Импортируй модель, чтобы SQLAlchemy "увидел" её
+from routers import users
+from routers import transactions
 
 
 # Создание таблиц в базе данных
 Base.metadata.create_all(bind=engine)
 print("✅ Таблицы созданы!")
-
 print("🚀 Приложение запущено")
-app = FastAPI()
+
+app = FastAPI(
+    title="Easy Plan API",
+    description="API для Easy Plan",
+    version="0.1.0",
+    openapi_tags=[{
+        "name": "users",
+        "description": "Операции с пользователями",
+    },
+    {
+        "name": "transactions",
+        "description": "Операции с транзакциями",
+    }]
+)
+
+# Подключаем роутер пользователей
+app.include_router(users.router, prefix="/api")
+app.include_router(transactions.router, prefix="/api")  # Добавляем роутер для транзакций
 
 @app.get("/")
 def read_root():
     return {"message": "Привет, мир!"}
 
-@app.get("/hello/{name}")
-def say_hello(name: str):
-    return {"message": f"Привет, {name}!"}
+
